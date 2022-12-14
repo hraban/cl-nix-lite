3f99b8b8c013a56615b7f099b2847372de9ecd20
Hraban Luyat <hraban@0brg.net>
2022-12-14 16:29:28 -0500

    make-binary depends on cffi setuphook now

--- examples/make-binary/default.nix
+++ examples/make-binary/default.nix
@@ -7,7 +7,7 @@ with lispPackagesLite;
 
 lispDerivation {
   # cl-async is a good litmus test dependency because it uses CFFI
-  lispDependencies = [ alexandria cl-async ];
+  lispDependencies = [ alexandria cl-async cffi ];
   lispSystem = "demo";
   version = "0.0.1";
   src = pkgs.lib.cleanSource ./.;
