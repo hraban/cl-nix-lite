82732ef3a3e8ef7f7e4bd2c5618f144f32f0501f
Hraban Luyat <hraban@0brg.net>
2023-05-07 02:29:03 -0400

    docs: don’t build asdf from src in example
    No need to, now that it’s part of the packages list.

--- examples/flake-app/flake.nix
+++ examples/flake-app/flake.nix
@@ -6,41 +6,24 @@
       flake = false;
       url = "github:hraban/cl-nix-lite";
     };
-    # This is how you would override a package or include a new one
-    asdf-src = {
-      url = "git+https://gitlab.common-lisp.net/asdf/asdf";
-      flake = false;
-    };
   };
   outputs = {
-    self, nixpkgs, asdf-src, cl-nix-lite, flake-utils
+    self, nixpkgs, cl-nix-lite, flake-utils
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lispPackagesLite = import cl-nix-lite { inherit pkgs; };
       in
-      with lispPackagesLite;
-      let
-        # Create a new lisp dependency on the fly from source. Note: this is not
-        # the same as overridePackage - for overriding a deeper dependency to
-        # automatically be picked up by other dependencies, make sure to see
-        # that example. This is just for adding an entirely new dependency.
-        asdf = lispDerivation {
-          src = asdf-src;
-          lispSystem = "asdf";
-        };
-      in
+        with lispPackagesLite;
         {
           packages = {
             default = lispDerivation {
               name = "flake-app";
               lispSystem = "flake-app";
               lispDependencies = [
-                # This is our own copy of asdf
-                asdf
-                # This came from the with lispPackagesLite scope
                 alexandria
+                asdf
                 arrow-macros
               ];
               src = pkgs.lib.cleanSource ./.;
