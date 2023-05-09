288b8dafb8f4d3f691d74e57afee16dc8997b6e1
Hraban Luyat <hraban@0brg.net>
2023-05-08 23:24:07 -0400

    build: don’t build any broken packages

--- examples/default.nix
+++ examples/default.nix
@@ -8,11 +8,30 @@
 
 let
   inherit (pkgs.lib) isDerivation;
-  allInputs = input:
+  # HACK: I can’t find a better / cleaner way to do this. It’s pretty
+  # frustrating. Problems with this solution:
+  #
+  # - it’s not recursive (for speed)
+  # - it doesn’t check all possible input sources (depsHostHost etc) (for
+  #   legibility)
+  # - It doesn’t catch any derivation included through string formatting
+  #   (e.g. "${pkgs.clisp}/bin/clisp ...").
+  #
+  # Ideally I’d have something like tryEval { ... } catch (isBroken) or
+  # whatever, but for some reason that’s not supported.
+  isBroken' = drv: drv.meta.broken;
+  isNotBroken = drv:
+    ! (
+      drv.meta.broken ||
+      builtins.any (drv: drv.meta.broken)
+                   (drv.buildInputs ++ drv.nativeBuildInputs)
+    );
+  # Extract all derivations from this path / set of derivations
+  allDerivations = input:
     if builtins.isPath input
     then
-      allInputs (pkgs.callPackage input { }) ++
-      # if CLISP is available on this system, and if the example allows
+      allDerivations (pkgs.callPackage input { }) ++
+      # If CLISP is available on this system, and if the example allows
       # overriding lispPackagesLite, import the example again but offer it an
       # argument of lispPackagesLite with its lisp set to CLISP.
       (pkgs.lib.optionals (!pkgs.clisp.meta.broken) [(
@@ -23,7 +42,7 @@ let
             };
           });
         in
-          allInputs (callPackage input { }))])
+          allDerivations (callPackage input { }))])
     else if isDerivation input
     then [ input ]
     else
@@ -35,11 +54,15 @@ in
 # to be /built/, on CI. That allows you to exclude anything that already exists
 # on cache. This is useful because otherwise it will redownload everything, just
 # to throw it away immediately again.
-pkgs.lib.lists.flatten (builtins.map allInputs [
+pkgs.lib.pipe [
   ./all-packages
   ./all-packages-wrapped
   ./hello-binary
   ./override-package
   ./test-all
   ./with-cffi
-])
+] [
+  (builtins.map allDerivations)
+  pkgs.lib.lists.flatten
+  (builtins.filter isNotBroken)
+]
