7cfc6a3ce28a6081af5abf2bca414ea429146480
Hraban Luyat <hraban@0brg.net>
2023-08-12 14:12:38 -0400

    syntax: fix function argument indenting

--- examples/test-all/default.nix
+++ examples/test-all/default.nix
@@ -1,45 +1,45 @@
 {
   pkgs ? import <nixpkgs> {}
-  , skip ? [
-    "_40ants-doc"
-    "_40ants-doc-full" # this one works in QL so it’s nix specific
-    "arnesi"
-    "bordeaux-threads" # There’s a deadlock heisenbug in these tests
-    "cffi"
-    "cl-difflib" # see https://github.com/wiseman/cl-difflib/pull/1
-    "cl-markdown"
-    "cl-redis"
-    "common-doc"
-    "dbi"
-    "dynamic-classes"
-    "fare-quasiquote"
-    "gettext" # I’m confused as to why this one is failing
-    "hamcrest"
-    "lack"
-    "lack-full"
-    "lack-request"
-    "lift"
-    "log4cl"
-    "log4cl-extras"
-    "moptilities"
-    "reblocks"
-    "routes"
-    "rutils"
-    "spinneret"
-    "str"
-    "trivial-backtrace"
-    "trivial-timeout"
-    "try"
-    "typo"
-    "with-output-to-stream"
-    "xlunit"
-  ] ++ pkgs.lib.optionals pkgs.hostPlatform.isDarwin [
-    "flexi-streams"
-    "lparallel"
-  ] ++ pkgs.lib.optionals pkgs.hostPlatform.isLinux [
-    "hunchentoot"
-    "usocket"
-  ]
+, skip ? [
+  "_40ants-doc"
+  "_40ants-doc-full" # this one works in QL so it’s nix specific
+  "arnesi"
+  "bordeaux-threads" # There’s a deadlock heisenbug in these tests
+  "cffi"
+  "cl-difflib" # see https://github.com/wiseman/cl-difflib/pull/1
+  "cl-markdown"
+  "cl-redis"
+  "common-doc"
+  "dbi"
+  "dynamic-classes"
+  "fare-quasiquote"
+  "gettext" # I’m confused as to why this one is failing
+  "hamcrest"
+  "lack"
+  "lack-full"
+  "lack-request"
+  "lift"
+  "log4cl"
+  "log4cl-extras"
+  "moptilities"
+  "reblocks"
+  "routes"
+  "rutils"
+  "spinneret"
+  "str"
+  "trivial-backtrace"
+  "trivial-timeout"
+  "try"
+  "typo"
+  "with-output-to-stream"
+  "xlunit"
+] ++ pkgs.lib.optionals pkgs.hostPlatform.isDarwin [
+  "flexi-streams"
+  "lparallel"
+] ++ pkgs.lib.optionals pkgs.hostPlatform.isLinux [
+  "hunchentoot"
+  "usocket"
+]
 }:
 
 with pkgs.lib;
