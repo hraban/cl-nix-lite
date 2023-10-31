e9a5be9ab57f950ddf9c8726699c56159964d2d1
Hraban Luyat <hraban@0brg.net>
2023-10-31 17:24:52 -0400

    build: cachix config in flake’s nixConfig

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
 {
   inputs = {};
 
+  nixConfig = {
+    extra-substituters = [ "https://cl-nix-lite.cachix.org" ];
+    extra-trusted-public-keys = [ "cl-nix-lite.cachix.org-1:ab6+b0u2vxymMLcZ5DDqPKnxz0WObbMszmC+BDBHpFc=" ];
+  };
+
   outputs = { self }: {
     overlays.default = import ./.;
   };
